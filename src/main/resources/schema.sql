4e1b257eb0a43516cc5b0ab3efcab6bc06e572c7
Daichi Yamashita <dahiyu07@gmail.com>
2016-12-31 17:35:24 +0900

    first

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS messages (
+	id		INT PRIMARY KEY AUTO_INCREMENT,
+	text	VARCHAR(255)
+);
+
+INSERT INTO messages (text) VALUES ('aaa');
+INSERT INTO messages (text) VALUES ('bbb');
+INSERT INTO messages (text) VALUES ('ccccc');
+INSERT INTO messages (text) VALUES ('d');
+INSERT INTO messages (text) VALUES ('e');
+INSERT INTO messages (text) VALUES ('f');
+INSERT INTO messages (text) VALUES ('g');
+INSERT INTO messages (text) VALUES ('h');
